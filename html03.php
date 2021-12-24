e30ca43eb970b8099cebb3845fe75b6207055236
apisitsa <apisit.sa@buu.ac.th>
2021-12-24 17:02:18 +0700

    sit

---
+++ html03.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>table</title>
+</head>
+<body>
+      <table border="1px" height="20">
+      <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+      </tr>
+      <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+      </tr>
+      <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+      </tr>
+      </table>
+</body>
+</html>
