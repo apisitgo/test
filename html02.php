e224acd4b31c6e5ed7976d01b372e1ad9dec13b2
apisitgo <96096002+apisitgo@users.noreply.github.com>
2021-12-24 17:31:40 +0700

    Update html02.php

--- html02.php
+++ html02.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>table</title>
-</head>
-<body>
-      <table border="1px" height="20">
-      <tr>
-            <td>1</td>
+q<!DOCTYPE html>
+q<html>
+q<head>
+q<title>table</title>
+q</head>
+q<body>
+ q     <table border="1px" height="20">
+ q     <tr>
+  q          <td>1</td>
             <td>2</td>
             <td>3</td>
       </tr>
